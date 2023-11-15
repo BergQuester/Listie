6ac0c5852501d1163d7d1a38f45d336f20f8ff3c
Daniel Bergquist <daniel.bergquist@hey.com>
2023-11-14 22:02:07 -0700

    Initial Commit

---
+++ Listie/ListieApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListieApp.swift
+//  Listie
+//
+//  Created by Daniel Bergquist on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ListieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
